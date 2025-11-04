39aface7d6b64d52a2e4d4c17c0a1299f1ff4ffc
rupalisarcar <rupalisarcar@gmail.com>
2025-11-05 00:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let priceAll = document.querySelectorAll(".price");
+	let totalPrice = 0;
+	priceAll.forEach(item=>totalPrice+=parseInt(item.innerText));
+	console.log(totalPrice);
+	const newRow= document.createElement('tr');
+	const firstCol = document.createElement('td')
+	firstCol.innerText = "Total Price";
+	const secCol = document.createElement('td')
+	secCol.innerText = `${totalPrice}`
+
+	newRow.appendChild(firstCol);
+	newRow.appendChild(secCol);
+
+	document.body.appendChild(newRow)
   
 };
 
